ae2dd65e115ead9bcec899869f3151bd1e5a7d23
mbe <basantraj@hotmail.com>
2026-04-16 14:15:59 +0600

    Fix workflow approval columns and remove service_logs dependency from /api/lifts

--- models/Customer.js
+++ models/Customer.js
@@ -1,28 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
-  const Site = sequelize.define(
-    'Site',
+  const Customer = sequelize.define(
+    'Customer',
     {
       id: { type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true },
       name: { type: DataTypes.TEXT, allowNull: false },
-      address: { type: DataTypes.TEXT, allowNull: true },
-      gpsLat: { type: DataTypes.DECIMAL(10, 7), allowNull: true, field: 'gps_lat' },
-      gpsLng: { type: DataTypes.DECIMAL(10, 7), allowNull: true, field: 'gps_lng' },
       createdAt: { type: DataTypes.DATE, allowNull: true, field: 'created_at' },
       updatedAt: { type: DataTypes.DATE, allowNull: true, field: 'updated_at' },
     },
     {
-      tableName: 'sites',
+      tableName: 'customers',
       timestamps: false,
     }
   );
 
-  Site.associate = (models) => {
-    Site.hasMany(models.Project, { foreignKey: 'site_id' });
+  Customer.associate = (models) => {
+    Customer.hasMany(models.Project, { foreignKey: 'customer_id' });
   };
 
-Customer.associate = (models) => {
-  Customer.hasMany(models.Project, { foreignKey: 'customer_id' });
-};
-
-  return Site;
+  return Customer;
 };
